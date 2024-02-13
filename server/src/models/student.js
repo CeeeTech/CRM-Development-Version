33f176b8893c26b58f9828aa256b1941dad0e701
isurika <153486985+CeeeTech@users.noreply.github.com>
2024-02-13 11:35:52 +0530

    date validation

--- server/src/models/student.js
+++ server/src/models/student.js
@@ -5,8 +5,16 @@ const studentSchema = new mongoose.Schema({
   nic: { type: String, required: false },
   dob: { type: Date, required: false },
   contact_no: String,
-  email: { type: String, required: false, unique: true},
+  email: { type: String, required: false, unique: true },
   address: String
+});
+
+// Add pre-save hook to handle "NaN-NaN-NaN" value for dob
+studentSchema.pre('save', function(next) {
+  if (this.dob === 'NaN-NaN-NaN') {
+    this.dob = null; // Set dob to null if it's "NaN-NaN-NaN"
+  }
+  next();
 });
 
 const Student = mongoose.model("Student", studentSchema);
